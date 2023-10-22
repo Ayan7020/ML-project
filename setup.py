7b3d515697e8df1e5fd54abfb10ffcf3807f1a29
Ayan Shaikh <ayanshaikh.pr@gmail.com>
2023-10-22 15:45:03 +0530

    Data Transformation

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPHEN_E_DOT = '-e .'
+HYPHEN_E_DOT = "-e ."
 
-def get_requirements(file_path:str) -> List[str]:
-    '''
+
+def get_requirements(file_path: str) -> List[str]:
+    """
     This function will return the list of requirements
-    '''
+    """
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace('\n','')  for req in requirements]
-        
+        requirements = [req.replace("\n", "") for req in requirements]
+
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
-            
+
     return requirements
 
+
 setup(
-    name = 'mlprojects',
-    version = '0.0.1',
-    author = 'Ayan Shaikh',
-    author_email = 'ayanshaikh.pr@gmail.com',
-    packages = find_packages(),
-    install_requires = get_requirements('requirements.txt')
+    name="mlprojects",
+    version="0.0.1",
+    author="Ayan Shaikh",
+    author_email="ayanshaikh.pr@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
 )
